08d8dd9577258da9fa6c8f89f3df586b92e2c47e
Nandhitha1807 <151927682+Nandhitha1807@users.noreply.github.com>
2024-02-29 10:50:10 +0530

    Create output2.c

---
+++ output2.c
@@ -0,0 +1,5 @@
+1.Convert the temperature from fahrenheit to celsius:
+2.Convert the temperature from celsius to fahrenheit:
+ Enter your choices(1,2): 2
+enter the temperature in celsius:100
+Temperature in Fahrenheit: 212.00
